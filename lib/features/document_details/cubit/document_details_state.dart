e992b5c51ef88b1decef3cb0cad4e3ea5faa15b2
Maximilian Fischer <github@maaeps.de>
2025-10-23 16:26:20 +0200

    revert: abstract Freezed

--- lib/features/document_details/cubit/document_details_state.dart
+++ lib/features/document_details/cubit/document_details_state.dart
@@ -1,7 +1,7 @@
 part of 'document_details_cubit.dart';
 
 @freezed
-sealed class DocumentDetailsState with _$DocumentDetailsState {
+abstract class DocumentDetailsState with _$DocumentDetailsState {
   const factory DocumentDetailsState({
     @Default(LoadingStatus.initial) LoadingStatus status,
     DocumentModel? document,
