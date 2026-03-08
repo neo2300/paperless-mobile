6af0571bdf4496699ba672e54b31d4c369c863a1
Anton Stubenbord <anton.stubenbord@protonmail.com>
2026-03-08 22:14:56 +0100

    fix: remove fontfamily from icondata

--- lib/helpers/custom_field_icon_mappings.dart
+++ lib/helpers/custom_field_icon_mappings.dart
@@ -4,14 +4,8 @@ import 'package:paperless_mobile/api/models/models.dart';
 IconData getCustomFieldIcon(DataTypeEnum dataType) {
   return switch (dataType) {
     DataTypeEnum.string => Icons.text_fields,
-    DataTypeEnum.integer => const IconData(
-      0x2115,
-      fontFamily: 'MaterialIcons',
-    ), //Natural numbers
-    DataTypeEnum.float => const IconData(
-      0x211D,
-      fontFamily: 'MaterialIcons',
-    ), //Real numbers
+    DataTypeEnum.integer => const IconData(0x2115), //Natural numbers
+    DataTypeEnum.float => const IconData(0x211D), //Real numbers
     DataTypeEnum.boolean => Icons.check_box,
     DataTypeEnum.date => Icons.calendar_today,
     DataTypeEnum.select => Icons.list,
