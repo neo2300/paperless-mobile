23c830b326882ab772ffb275fcb84adfda59f127
Anton Stubenbord <anton.stubenbord@protonmail.com>
2026-03-14 23:59:57 +0100

    chore: bump version

--- lib/features/changelogs/view/changelog_dialog.dart
+++ lib/features/changelogs/view/changelog_dialog.dart
@@ -54,7 +54,7 @@ class ChangelogDialog extends StatelessWidget {
 }
 
 const _versionNumbers = {
-  "6403": "4.3.0",
+  "6503": "4.3.1",
   "6203": "4.2.0-apikey-auth-rc1",
   "6103": "4.1.0",
   "6003": "4.0.6",
