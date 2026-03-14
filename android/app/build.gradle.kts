f9931a611056439fa23e11da9c4ae9c397f71622
Anton Stubenbord <anton.stubenbord@protonmail.com>
2026-03-14 23:27:04 +0100

    fix: use release signing config for release builds

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -66,7 +66,7 @@ android {
 
     buildTypes {
         getByName("release") {
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
         getByName("debug") {
             applicationIdSuffix = ".debug"
